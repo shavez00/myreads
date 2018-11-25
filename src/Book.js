c7d7389b80ad67055112ddc4c9d6c56dbddc4850
shavez00@yahoo.com <shavez00@IP-C613647C.corp.amazonworkspaces.com>
2018-11-24 22:52:15 -0800

    Update SearchPage, MainPage, Book componets so that they work

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,28 @@
-import React, {Componet} from 'react';
+import React, { Component } from 'react'
 
-class Book extends React.Component {
+class Book extends Component {
   render() {
-  
-  }
 
-export default Book;
+    return (
+        <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: '")' }}></div>
+                <div className="book-shelf-changer">
+                  <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title"></div>
+              <div className="book-authors"></div>
+            </div>
+    )
+  }
+}
+
+export default Book
 
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,19 +1,20 @@
-import React, {Componet} from 'react';
+import React, { Component } from 'react';
 
-class SearchPage extends React.Component {
+class SearchPage extends Component {
   render() {
-      <div className="search-books">
+    return (
+          <div className="search-books">
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
                 {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                 NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                 You can find these search terms here:
+                 https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
+                 However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                 you don't find a specific author or title. Every search is limited by search terms.
+                 */}
                 <input type="text" placeholder="Search by title or author"/>
 
               </div>
@@ -22,7 +23,8 @@ class SearchPage extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
+    );
   }
+}
 
-export default SearchPage;
-
+export default SearchPage
