ea1d2e28969c12056c002c2aeca59d4bf4dcc76c
shavez00@yahoo.com <shavez00@IP-C613647C.corp.amazonworkspaces.com>
2018-11-25 10:23:49 -0800

    fixed bug where currentShelf is not selected which broke moving a book to the currentlyReading shelf

--- src/MainPage.js
+++ src/MainPage.js
@@ -23,6 +23,7 @@ class MainPage extends Component {
                                     <Book 
                                         book={book}
                                         moveShelf={this.props.moveShelf}
+                                        currentShelf="currentlyReading"
                                     />
                                 </li>
                             ))
@@ -41,6 +42,7 @@ class MainPage extends Component {
                                     <Book 
                                         book={book}
                                         moveShelf={this.props.moveShelf}
+                                        currentShelf="wantToRead"       
                                     />
                                 </li>
                             ))
@@ -59,6 +61,7 @@ class MainPage extends Component {
                                     <Book 
                                         book={book}
                                         moveShelf={this.props.moveShelf}
+                                        currentShelf="read"   
                                     />
                                 </li>
                             ))
