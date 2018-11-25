d857ceae2067503d7d0ab903c2db134025b946fe
shavez00@yahoo.com <shavez00@IP-C613647C.corp.amazonworkspaces.com>
2018-11-25 09:31:49 -0800

    added the functionality to pull images, title, and author from BookAPI

--- src/MainPage.js
+++ src/MainPage.js
@@ -20,7 +20,9 @@ class MainPage extends Component {
                             .filter(book => book.shelf === 'currentlyReading')
                             .map(book => (
                                 <li key= {book.id} >
-                                    <Book />
+                                    <Book 
+                                        book={book}
+                                    />
                                 </li>
                             ))
                     }
@@ -35,7 +37,9 @@ class MainPage extends Component {
                             .filter(book => book.shelf === 'wantToRead')
                             .map(book => (
                                 <li key= {book.id} >
-                                    <Book />
+                                    <Book 
+                                        book={book}
+                                    />
                                 </li>
                             ))
                     }
@@ -50,7 +54,9 @@ class MainPage extends Component {
                             .filter(book => book.shelf === 'read')
                             .map(book => (
                                 <li key= {book.id} >
-                                    <Book />
+                                    <Book 
+                                        book={book}
+                                    />
                                 </li>
                             ))
                     }
