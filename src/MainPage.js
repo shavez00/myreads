62afc47b80f16cc1ade893d3e1e0e1337c274d14
shavez00@yahoo.com <shavez00@IP-C613647C.corp.amazonworkspaces.com>
2018-11-25 08:43:30 -0800

    Updated MainPage so that all three shelves are now pulling from BooksAPI

--- src/MainPage.js
+++ src/MainPage.js
@@ -16,9 +16,14 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
+                      {this.props.books
+                            .filter(book => book.shelf === 'currentlyReading')
+                            .map(book => (
+                                <li key= {book.id} >
+                                    <Book />
+                                </li>
+                            ))
+                    }
                     </ol>
                   </div>
                 </div>
@@ -26,9 +31,14 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
+                    {this.props.books
+                            .filter(book => book.shelf === 'wantToRead')
+                            .map(book => (
+                                <li key= {book.id} >
+                                    <Book />
+                                </li>
+                            ))
+                    }
                     </ol>
                   </div>
                 </div>
@@ -36,9 +46,14 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
+                      {this.props.books
+                            .filter(book => book.shelf === 'read')
+                            .map(book => (
+                                <li key= {book.id} >
+                                    <Book />
+                                </li>
+                            ))
+                    }
                     </ol>
                   </div>
                 </div>
