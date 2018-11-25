f8f2a622b15398fd0d3238ff6a9928f7f188b48c
shavez00@yahoo.com <shavez00@IP-C613647C.corp.amazonworkspaces.com>
2018-11-25 10:14:11 -0800

    fixed bug requiring user to refresh browser when a book is moved between shelves.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class BooksApp extends React.Component {
     
     moveShelf = (book, shelf) => {
         BooksAPI.update(book, shelf);
+        
+        BooksAPI.getAll().then((books) => {
+            this.setState({ books })
+        })
     }
     
   render() {
